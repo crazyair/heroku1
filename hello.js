e074315063444c2a3df80288bb0a812fcc76e34d
黄建峰 <645381995@qq.com>
2016-08-16 09:00:26 +0800

    'my'

---
+++ hello.js
@@ -0,0 +1,30 @@
+// var koa = require('koa');
+// var app = koa();
+
+// app.use(function *(){
+//   this.body = 'Hello World';
+// });
+
+// app.listen(3000);
+
+var koa = require('koa');
+var app = koa();
+
+// logger
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log(1111);
+  console.log('%s %s - %s', this.method, this.url, ms);
+  console.log(2222);
+});
+
+// response
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
